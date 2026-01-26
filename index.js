11df4351fc5687d2edb693e5f05ae8191bf6a7a9
Andrea Ho <anhhothai.iamcoder@gmail.com>
2026-01-26 20:01:36 +0700

    feat: Reading & Parsing a Feed

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
+import Parser from "rss-parser";
+
 async function main() {
+  const parser = new Parser();
+
   const URL = "https://www.bonappetit.com/feed/recipes-rss-feed/rss";
 
   try {
-    const response = await fetch(URL);
-    console.log(await response.text());
+    const { title, items } = await parser.parseURL(URL);
+    console.log(title);
+
+    const results = items.map(({ title, link }) => ({ title, link }));
+    console.table(results);
   } catch (error) {
     throw new Error(error);
   }
